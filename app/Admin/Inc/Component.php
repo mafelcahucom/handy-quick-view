023b30bf6a07ba8e1f746495659bc19994541a90
mafelcahucom <mafeljohn.timkang.cahucom070596@gmail.com>
2022-10-07 16:09:26 +0800

    50

--- app/Admin/Inc/Component.php
+++ app/Admin/Inc/Component.php
@@ -36,7 +36,7 @@ final class Component {
      * @return HTMLElement
      */
     public static function get_logo() {
-        return '<img class="hd-logo" src="'. Helper::get_asset_src( 'images/logo.svg' ) .'" alt="Handy Sliding Cart" title="Handy Sliding Cart">';
+        return '<img class="hd-logo" src="'. Helper::get_asset_src( 'images/logo.svg' ) .'" alt="Handy Quick View" title="Handy Quick View">';
     }
 
     /**
