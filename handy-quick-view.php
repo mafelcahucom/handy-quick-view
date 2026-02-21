bae2fd3682eae4f0095a23c30a3ff0b529e50c67
Mafel John Cahucom <53706680+mafelcahucom@users.noreply.github.com>
2026-02-21 14:51:18 +0800

    Update handy-quick-view.php

--- handy-quick-view.php
+++ handy-quick-view.php
@@ -9,7 +9,7 @@
  * Domain Path:          /languages
  * Requires at least:    5.8
  * WC requires at least: 5.0.0
- * License:              GPLv2 or later
+ * License:              GPLv3 or later
  */
 
 defined( 'ABSPATH' ) || exit;
